b55f217151e2e7242ce628108c164540e96f9457
Louis Medina <louisfm16@gmail.com>
2018-09-25 21:52:57 -0700

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 // External Imports
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -71,6 +72,34 @@ app.delete('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     }).catch((e) => {  // Error
         // 400 w/ empty body
+        res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        // true
+        body.completedAt = new Date().getTime();
+    } else {
+        // false
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => {
         res.status(400).send();
     });
 });
