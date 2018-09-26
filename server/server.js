5d5bd3aa03d28cfb5cc8d3adc4c6298aa6d681cf
Louis Medina <louisfm16@gmail.com>
2018-09-25 19:50:11 -0700

    Added GET /todos/:id test

--- server/server.js
+++ server/server.js
@@ -37,17 +37,17 @@ app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
     if(!ObjectID.isValid(id)) {
-        return res.status(404).send({});
+        return res.status(404).send();
     }
 
     Todo.findById(id).then((todo) => {
         if(!todo) {
-            return res.status(404).send({});
+            return res.status(404).send();
         }
 
         res.send({todo: todo});
-    }).catach((e) => {
-        res.status(400).send({});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
