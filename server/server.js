f6ae8a7db45a3ab6adb82cdf2906a4fbcfc33820
Louis Medina <louisfm16@gmail.com>
2018-09-25 20:04:45 -0700

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -32,7 +33,6 @@ app.get('/todos', (req, res) => {
     });
 });
 
-// GET /todos/id(2315sadjshadk323e4)
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -51,8 +51,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Started @ http://localhost:3000/');
+app.listen(port, () => {
+    console.log(`Started @ port: ${port}`);
 });
 
 module.exports = {
